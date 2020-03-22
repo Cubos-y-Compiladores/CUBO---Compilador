5eac08d0234333c958310ae580aeccdc7c9fbea9
Garza <danni900g@gmail.com>
2020-03-22 14:22:48 -0600

    Se insertan listas,matrices y cubos

--- GUI.py
+++ GUI.py
@@ -46,14 +46,14 @@ class MyApp(wx.Frame):
         self.sub8x8 = self.subMenuMat2D.Append(8, "8x8")
         self.subInsertM2 = self.subMenuInsert.AppendSubMenu(self.subMenuMat2D,"Matriz2D")
 
-        self.sub1x1x1 = self.subMenuMat3D.Append(1, "1x1x1")
-        self.sub2x2x2 = self.subMenuMat3D.Append(2, "2x2x2")
-        self.sub3x3x3 = self.subMenuMat3D.Append(3, "3x3x3")
-        self.sub4x4x4 = self.subMenuMat3D.Append(4, "4x4x4")
-        self.sub5x5x5 = self.subMenuMat3D.Append(5, "5x5x5")
-        self.sub6x6x6 = self.subMenuMat3D.Append(6, "6x6x6")
-        self.sub7x7x7 = self.subMenuMat3D.Append(7, "7x7x7")
-        self.sub8x8x8 = self.subMenuMat3D.Append(8, "8x8x8")
+        self.sub1x1x1 = self.subMenuMat3D.Append(11, "1x1x1")
+        self.sub2x2x2 = self.subMenuMat3D.Append(22, "2x2x2")
+        self.sub3x3x3 = self.subMenuMat3D.Append(33, "3x3x3")
+        self.sub4x4x4 = self.subMenuMat3D.Append(44, "4x4x4")
+        self.sub5x5x5 = self.subMenuMat3D.Append(55, "5x5x5")
+        self.sub6x6x6 = self.subMenuMat3D.Append(66, "6x6x6")
+        self.sub7x7x7 = self.subMenuMat3D.Append(77, "7x7x7")
+        self.sub8x8x8 = self.subMenuMat3D.Append(88, "8x8x8")
         self.subInsertM3 = self.subMenuInsert.AppendSubMenu(self.subMenuMat3D,"Matriz3D")
 
         self.subRun = self.subMenuRun.Append(-1,"Run This\tCtrl-F5")
@@ -65,8 +65,6 @@ class MyApp(wx.Frame):
         self.SetMenuBar(self.mainMenu)
         # Eventos menu
         self.Bind(wx.EVT_MENU, self.insertList, self.subInsertL)
-        self.Bind(wx.EVT_MENU, self.insertMatriz2, self.subInsertM2)
-        self.Bind(wx.EVT_MENU, self.insertMatriz3, self.subInsertM3)
         self.Bind(wx.EVT_MENU, self.subExitWindow, self.subExit)
         self.Bind(wx.EVT_MENU, self.openFileCBC , self.subOpenCBC)
         self.Bind(wx.EVT_MENU, self.openFileTXT, self.subOpenTXT)
@@ -74,18 +72,31 @@ class MyApp(wx.Frame):
         self.Bind(wx.EVT_MENU, self.newFile, self.subNew)
         self.Bind(wx.EVT_MENU, self.runFile, self.subRun)
 
+
+
+
         # Eventos Submenus de matrices 2D
 
-        self.Bind(wx.EVT_MENU, partial(self.insertMatriz2,1), self.sub1x1)
-        self.Bind(wx.EVT_MENU, partial(self.insertMatriz2,2), self.sub2x2)
-        self.Bind(wx.EVT_MENU, partial(self.insertMatriz2,3), self.sub3x3)
-        self.Bind(wx.EVT_MENU, partial(self.insertMatriz2,4), self.sub4x4)
-        self.Bind(wx.EVT_MENU, partial(self.insertMatriz2,5), self.sub5x5)
-        self.Bind(wx.EVT_MENU, partial(self.insertMatriz2,6), self.sub6x6)
-        self.Bind(wx.EVT_MENU, partial(self.insertMatriz2,7), self.sub7x7)
-        self.Bind(wx.EVT_MENU, partial(self.insertMatriz2,8), self.sub8x8)
+        self.Bind(wx.EVT_MENU, partial(self.insertMatriz2D,1), self.sub1x1)
+        self.Bind(wx.EVT_MENU, partial(self.insertMatriz2D,2), self.sub2x2)
+        self.Bind(wx.EVT_MENU, partial(self.insertMatriz2D,3), self.sub3x3)
+        self.Bind(wx.EVT_MENU, partial(self.insertMatriz2D,4), self.sub4x4)
+        self.Bind(wx.EVT_MENU, partial(self.insertMatriz2D,5), self.sub5x5)
+        self.Bind(wx.EVT_MENU, partial(self.insertMatriz2D,6), self.sub6x6)
+        self.Bind(wx.EVT_MENU, partial(self.insertMatriz2D,7), self.sub7x7)
+        self.Bind(wx.EVT_MENU, partial(self.insertMatriz2D,8), self.sub8x8)
 
-        # Eventos Submenus de matrices 3D
+        self.Bind(wx.EVT_MENU, partial(self.insertMatriz3D,11), self.sub1x1x1)
+        self.Bind(wx.EVT_MENU, partial(self.insertMatriz3D,22), self.sub2x2x2)
+        self.Bind(wx.EVT_MENU, partial(self.insertMatriz3D,33), self.sub3x3x3)
+        self.Bind(wx.EVT_MENU, partial(self.insertMatriz3D,44), self.sub4x4x4)
+        self.Bind(wx.EVT_MENU, partial(self.insertMatriz3D,55), self.sub5x5x5)
+        self.Bind(wx.EVT_MENU, partial(self.insertMatriz3D,66), self.sub6x6x6)
+        self.Bind(wx.EVT_MENU, partial(self.insertMatriz3D,77), self.sub7x7x7)
+        self.Bind(wx.EVT_MENU, partial(self.insertMatriz3D,88), self.sub8x8x8)
+
+
+
 
         # Botones
         btn1 = wx.Button(self,-1,u"B",pos=(0,4),size=(20,20))
@@ -132,22 +143,54 @@ class MyApp(wx.Frame):
     def insertList(self,event):
         self.textMain.AppendText("list" + str(self.contList) +"= [];\n")
         self.contList += 1
-    def insertMatriz2(self,number,event):
+    def insertMatriz2D(self,number,event):
 
         text = "["
         for i in range(number-1):
-            text += "[],"*number
-            text += "\n "
-        text += "[]"*number
-        text+="\n ];"
+            text+="[],\n\t     "
 
-        print(text)
-        # self.textMain.AppendText("matriz2D" + str(self.contMatriz2) +"= [[],[]];\n")
-        # self.contMatriz2 += 1
-    def insertMatriz3(self,num,event):
-        # self.textMain.AppendText("matriz3D" + str(self.contMatriz3) +"= [[[],[]],[[],[]]];\n")
-        # self.contMatriz3 += 1
-        print(num)
+        text += "[]];\n"
+
+            # text += "["
+            # text += "False,"*(number-1)
+            # text += "Fase],"
+            # text += "\n\t     "
+        # text += "["
+        # text += "False,"*(number-1)
+        # text += "False]"
+        # text+="];\n"
+
+        self.textMain.AppendText("matriz2D" + str(self.contMatriz2) +"= "+ text)
+        self.contMatriz2 += 1
+    def insertMatriz3D(self,number,event):
+        number = number%10
+        textFinal = "["
+
+        for i in range(number-1):
+            text = "["
+
+            for j in range(number - 1):
+                text += "[],"
+
+
+            text += "[]"
+            text += "]"
+            textFinal += text+",\n\t     "
+
+        text = "["
+        for k in range(number - 1):
+            text += "[],"
+
+
+        text += "[]"
+        text += "]"
+        textFinal += text+"];\n"
+        print(textFinal)
+
+        self.textMain.AppendText("matriz3D" + str(self.contMatriz3) +"= "+ textFinal)
+        self.contMatriz3 += 1
+
+
     def subExitWindow(self,event):
         self.Close(1)
     def openFileCBC(self,event):
